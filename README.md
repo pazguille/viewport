49c31e09a809a41f01e911d859d4dedd8ed54a22
pazguille <guille87paz@gmail.com>
2012-11-08 02:13:06 -0300

    Create component. Update properties in component.json

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-viewport
-========
+# Viewport Component
 
-Gets the dimensions of the Viewport 
+Gets the dimensions of the Viewport and beyond
+
+## Installation
+
+    $ component install pazguille/viewport
+
+## API
+
+## Example
+```javascript
+
+```
+
+## Contact
+- Guille Paz (Frontend developer - JavaScript developer | Web standards lover)
+- E-mail: [guille87paz@gmail.com](mailto:guille87paz@gmail.com)
+- Twitter: [@pazguille](http://twitter.com/pazguille)
+- Web: [http://pazguille.me](http://pazguille.me)
+
+## License
+### The MIT License
+Copyright (c) 2012 [@pazguille](http://twitter.com/pazguille)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,143 @@
+/**
+ * Module dependencies.
+ */
+var Emitter = require('emitter'),
+	win = window,
+	doc = win.document,
+	docEl = doc.documentElement,
+	on = win.addEventListener || win.attachEvent,
+	RESIZE = (win.attachEvent) ? 'onresize' : 'resize',
+	SCROLL = (win.attachEvent) ? 'onscroll' : 'scroll',
+	resized = false,
+	scrolled = false;
+
+function update() {
+	// No changing, no execution
+	if (!resized && !scrolled) { return; }
+
+	var eve = (resized) ? 'resize' : 'scroll';
+
+	// Updates viewport position
+	this.refresh();
+
+	// Change status
+	resized = false;
+	scrolled = false;
+
+	this.emit(eve);
+}
+
+/**
+ * Events
+ */
+on(RESIZE, function () { resized = true; });
+on(SCROLL, function () { scrolled = true; });
+
+/**
+ * Viewport
+ */
+function Viewport() {
+	this.init();
+}
+
+Emitter(Viewport.prototype);
+
+Viewport.prototype.init = function () {
+	var that = this;
+
+	that.calculateDimensions();
+	that.calculateDeviceDimensions();
+	that.calculateScroll();
+	that.calculateOffset();
+	that.calculateOrientation();
+
+	win.setInterval(function () {
+		update.call(that);
+	}, 350);
+
+};
+
+Viewport.prototype.device = {};
+Viewport.prototype.document = {};
+Viewport.prototype.cordinate = {};
+
+Viewport.prototype.calculateDimensions = function () {
+	this.height = docEl.clientHeight;
+	this.width = docEl.clientWidth;
+};
+
+Viewport.prototype.calculateDeviceDimensions = function () {
+	this.device.height = win.screen.height;
+	this.device.width = win.screen.width;
+};
+
+Viewport.prototype.calculateScroll = function () {
+	this.scrollY = win.pageYOffset || docEl.scrollTop;
+	this.scrollX = win.pageXOffset || docEl.scrollLeft;
+
+	if (this.scrollY === 0) {
+		this.emit('top');
+
+	} else if ((this.height + this.scrollY) === doc.body.scrollHeight) {
+		this.emit('bottom');
+	}
+
+};
+
+Viewport.prototype.calculateOffset = function () {
+	this.top = this.scrollY;
+	this.right = this.scrollX + this.width;
+	this.bottom = this.scrollY + this.height;
+	this.left = this.scrollX;
+};
+
+
+Viewport.prototype.calculateEventcoordinates = function (eve) {
+	// add support for touch events ==> eve.changedTouches;
+	var coordX = 0,
+		coordY = 0;
+
+	eve = eve || window.event;
+
+	if (eve.pageX || eve.pageY) {
+		coordX = eve.pageX;
+		coordY = eve.pageY;
+
+	} else {
+		coordX = eve.clientX + doc.body.scrollLeft + docEl.scrollLeft;
+		coordY = eve.clientY + doc.body.scrollTop + docEl.scrollTop;
+	}
+
+	return {
+		'posX': coordX,
+		'posY': coordY
+	};
+};
+
+Viewport.prototype.calculateOrientation = function () {
+	this.orientation = (Math.abs(win.orientation) === 90) ? 'landscape' : 'portrait';
+};
+
+Viewport.prototype.inViewport = function (el) {
+	var r = el.getBoundingClientRect();
+
+	return (r.top > 0) && (r.right < this.width) && (r.bottom < this.height) && (r.left > 0);
+};
+
+Viewport.prototype.inVisible = function (el) {
+	var r = el.getBoundingClientRect();
+
+	return (r.height >= this.top);
+};
+
+Viewport.prototype.refresh = function () {
+	this.calculateDimensions();
+	this.calculateScroll();
+	this.calculateOffset();
+	this.calculateOrientation();
+};
+
+/**
+ * Expose Viewport
+ */
+exports = module.exports = Viewport;
