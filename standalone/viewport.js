d56ebf3e2a6ee1277d90aa2a66263defce305125
Guille Paz <guille87paz@gmail.com>
2013-07-12 08:39:54 -0300

    Build standalone version.

--- standalone/viewport.js
+++ standalone/viewport.js
@@ -1,4 +1,4 @@
-(function(e){"use strict";function i(){this.collection={};this.maxListeners=10}var t=e.Object,n=e.Array,r=function(){if(typeof n.isArray==="function"){return n.isArray}return function(e){return t.prototype.toString.call(e)==="[object Array]"}}();i.prototype.addListener=function(e,t,n){if(e===undefined){throw new Error('jvent - "addListener(event, listener)": It should receive an event.')}if(t===undefined){throw new Error('jvent - "addListener(event, listener)": It should receive a listener function.')}var r=this.collection;t.once=n||false;if(r[e]===undefined){r[e]=[]}if(r[e].length+1>this.maxListeners&&this.maxListeners!==0){throw new Error("Warning: So many listeners for an event.")}r[e].push(t);this.emit("newListener");return this};i.prototype.on=i.prototype.addListener;i.prototype.once=function(e,t){this.on(e,t,true);return this};i.prototype.removeListener=function(e,t){if(e===undefined){throw new Error('jvent - "removeListener(event, listener)": It should receive an event.')}if(t===undefined){throw new Error('jvent - "removeListener(event, listener)": It should receive a listener function.')}var n=this.collection[e],i=0,s;if(r(n)){s=n.length;for(i;i<s;i+=1){if(n[i]===t){n.splice(i,1);break}}}return this};i.prototype.off=i.prototype.removeListener;i.prototype.removeAllListeners=function(e){if(e===undefined){throw new Error('jvent - "removeAllListeners(event)": It should receive an event.')}delete this.collection[e];return this};i.prototype.setMaxListeners=function(e){if(isNaN(e)){throw new Error('jvent - "setMaxListeners(n)": It should receive a number.')}this.maxListeners=e;return this};i.prototype.listeners=function(e){if(e===undefined){throw new Error('jvent - "listeners(event)": It should receive an event.')}if(this.collection[e]===undefined){throw new Error('jvent - "listeners(event)": The event must exist into the collection.')}return this.collection[e]};i.prototype.emit=function(){var e=arguments,t=e[0],n,i,s;if(t===undefined){throw new Error('jvent - "emit(event)": It should receive an event.')}if(typeof t==="string"){t={type:t}}if(!t.target){t.target=this}if(r(this.collection[t.type])){n=this.collection[t.type];i=0;s=n.length;for(i;i<s;i+=1){n[i].apply(this,arguments);if(n[i].once){this.off(t.type,n[i]);s-=1;i-=1}}}return this};if(typeof e.define==="function"&&e.define.amd!==undefined){e.define("Jvent",[],function(){return i})}else if(typeof module!=="undefined"&&module.exports!==undefined){module.exports=i}else{e.Jvent=e.EventEmitter=i}})(this);
+(function(e){"use strict";function t(){this.collection={};this.maxListeners=10}t.prototype.addListener=function(e,t,n){if(e===undefined){throw new Error('jvent - "addListener(event, listener)": It should receive an event.')}if(t===undefined){throw new Error('jvent - "addListener(event, listener)": It should receive a listener function.')}var r=this.collection;t.once=n||false;if(r[e]===undefined){r[e]=[]}if(r[e].length+1>this.maxListeners&&this.maxListeners!==0){throw new Error("Warning: So many listeners for an event.")}r[e].push(t);this.emit("newListener");return this};t.prototype.on=t.prototype.addListener;t.prototype.once=function(e,t){this.on(e,t,true);return this};t.prototype.removeListener=function(e,t){if(e===undefined){throw new Error('jvent - "removeListener(event, listener)": It should receive an event.')}if(t===undefined){throw new Error('jvent - "removeListener(event, listener)": It should receive a listener function.')}var n=this.collection[e],r=0,i;if(n!==undefined){i=n.length;for(r;r<i;r+=1){if(n[r]===t){n.splice(r,1);break}}}return this};t.prototype.off=t.prototype.removeListener;t.prototype.removeAllListeners=function(e){if(e===undefined){throw new Error('jvent - "removeAllListeners(event)": It should receive an event.')}delete this.collection[e];return this};t.prototype.setMaxListeners=function(e){if(isNaN(e)){throw new Error('jvent - "setMaxListeners(n)": It should receive a number.')}this.maxListeners=e;return this};t.prototype.listeners=function(e){if(e===undefined){throw new Error('jvent - "listeners(event)": It should receive an event.')}return this.collection[e]};t.prototype.emit=function(){var e=Array.prototype.slice.call(arguments,0),t=e.shift(),n,r=0,i;if(t===undefined){throw new Error('jvent - "emit(event)": It should receive an event.')}if(typeof t==="string"){t={type:t}}if(!t.target){t.target=this}if(this.collection[t.type]!==undefined){n=this.collection[t.type];i=n.length;for(r;r<i;r+=1){n[r].apply(this,e);if(n[r].once){this.off(t.type,n[r]);i-=1;r-=1}}}return this};if(typeof e.define==="function"&&e.define.amd!==undefined){e.define("Jvent",[],function(){return t})}else if(typeof module!=="undefined"&&module.exports!==undefined){module.exports=t}else{e.Jvent=e.EventEmitter=t}})(this);
 (function (window) {
     'use strict';
 
@@ -8,15 +8,25 @@
     var win = window,
         doc = win.document,
         docEl = doc.documentElement,
+        Jvent = win.Jvent,
         on = win.addEventListener || win.attachEvent,
-        RESIZE = (win.attachEvent) ? 'onresize' : 'resize',
-        SCROLL = (win.attachEvent) ? 'onscroll' : 'scroll',
+        RESIZE = (on === win.attachEvent) ? 'onresize' : 'resize',
+        SCROLL = (on === win.attachEvent) ? 'onscroll' : 'scroll',
         resized = false,
-        scrolled = false;
+        scrolled = false,
+        requestAnimFrame = (function () {
+            return window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                function (callback) {
+                    window.setTimeout(callback, 1000 / 60);
+                };
+        }());
 
     function update() {
+
         // No changing, exit
-        if (!resized && !scrolled) { return; }
+        if (!resized && !scrolled) { return; }
 
         var eve = resized ? 'resize' : 'scroll';
 
@@ -37,8 +47,9 @@
     on(RESIZE, function () { resized = true; });
     on(SCROLL, function () { scrolled = true; });
 
+
     /**
-     * Viewport
+     * Viewport class
      */
     function Viewport() {
 
@@ -52,6 +63,8 @@
         }
 
         this.init();
+
+        return this;
     }
 
     Viewport.prototype = new Jvent();
@@ -62,11 +75,14 @@
         that.refresh();
         that.calculateDeviceDimensions();
 
-        win.setInterval(function () {
+        (function updateloop() {
+            requestAnimFrame(updateloop);
             update.call(that);
-        }, 350);
+        }());
 
         Viewport.getInstance = this;
+
+        return this;
     };
 
     Viewport.prototype.device = {};
@@ -74,11 +90,15 @@
     Viewport.prototype.calculateDimensions = function () {
         this.height = docEl.clientHeight;
         this.width = docEl.clientWidth;
+
+        return this;
     };
 
     Viewport.prototype.calculateDeviceDimensions = function () {
         this.device.height = win.screen.height;
         this.device.width = win.screen.width;
+
+        return this;
     };
 
     Viewport.prototype.calculateScroll = function () {
@@ -96,6 +116,8 @@
         } else if (cachedBottom !== bottom && bottom >= doc.body.scrollHeight) {
             this.emit('bottom');
         }
+
+        return this;
     };
 
     Viewport.prototype.calculateOffset = function () {
@@ -103,15 +125,16 @@
         this.right = this.scrollX + this.width;
         this.bottom = this.scrollY + this.height;
         this.left = this.scrollX;
-    };
 
+        return this;
+    };
 
     Viewport.prototype.calculateMousePostition = function (eve) {
         // add support for touch events ==> eve.changedTouches;
         var coordX = 0,
             coordY = 0;
 
-        eve = eve || window.event;
+        eve = eve || window.event;
 
         if (eve.pageX || eve.pageY) {
             coordX = eve.pageX;
@@ -126,19 +149,22 @@
             'posX': coordX,
             'posY': coordY
         };
+
     };
 
     Viewport.prototype.calculateOrientation = function () {
         this.orientation = (Math.abs(win.orientation) === 90)
             ? 'landscape'
             : 'portrait';
+
+        return this;
     };
 
     Viewport.prototype.inViewport = function (el) {
         var r = el.getBoundingClientRect();
 
-        return (r.top > 0) && (r.right < this.width)
-            && (r.bottom < this.height) && (r.left > 0);
+        return (r.top >= 0) && (r.right <= this.width)
+            && (r.bottom <= this.height) && (r.left >= 0);
     };
 
     Viewport.prototype.isVisible = function (el) {
@@ -152,6 +178,8 @@
         this.calculateScroll();
         this.calculateOffset();
         this.calculateOrientation();
+
+        return this;
     };
 
     /**
