68556bbbb146c4a97f8e122a4df17bd4ddf9d4fb
Guille Paz <guille87paz@gmail.com>
2013-08-19 20:44:09 -0300

    Fix isVisible() method.

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ Viewport.prototype.inViewport = function (el) {
 Viewport.prototype.isVisible = function (el) {
     var r = el.getBoundingClientRect();
 
-    return (r.height >= this.top);
+    return (r.bottom >= 0 && r.top <= this.height);
 };
 
 Viewport.prototype.refresh = function () {
